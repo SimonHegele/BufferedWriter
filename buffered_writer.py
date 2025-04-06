b60408d9a341c7ffd659deb975c663e533fd3ceb
Simon Hegele <103111576+SimonHegele@users.noreply.github.com>
2025-04-06 09:31:24 +0200

    Update buffered_writer.py

--- buffered_writer.py
+++ buffered_writer.py
@@ -1,5 +1,5 @@
-from os   import path
-from time import sleep
+from collections import defaultdict
+from os          import path
 
 class BufferedWriter():
     """
@@ -9,7 +9,7 @@ class BufferedWriter():
     files the BufferedWriter reduces computation time.
     """
 
-    def __init__(self, out_dir=".", lines_per_file=25):
+    def __init__(self, out_dir=".", lines_per_file=25) -> None:
         """
         Args:
             out_dir (str, optional):        Base path for all files you want to write to.
@@ -18,7 +18,7 @@ class BufferedWriter():
                                             Defaults to 25.
         """
 
-        self.buffer          = {}
+        self.buffer          = defaultdict(list)
         self.lines_per_file  = lines_per_file
         self.out_dir         = out_dir
 
@@ -28,7 +28,7 @@ class BufferedWriter():
     def __exit__(self, exec_type, exec_val, exec_tb):
         self.flush()
 
-    def flush_file(self, file_path: str):
+    def flush_file(self, file_path: str) -> None:
         """
         Write buffered content to chosen file
 
@@ -41,14 +41,14 @@ class BufferedWriter():
                 file.write(line+"\n")
         self.buffer[file_path] = []
 
-    def flush(self):
+    def flush(self) -> None:
         """
         Write all buffered content to their respective files
         """
         for file_path in self.buffer.keys():
             self.flush_file(file_path)
 
-    def write_line(self, file_path: str, line: str):
+    def write_line(self, file_path: str, line: str) -> None:
         """
         Add a line to the buffer
 
@@ -56,10 +56,7 @@ class BufferedWriter():
             file_path (str):
             line (str):
         """
-        try:
-            self.buffer[file_path].append(line)
-        except:
-            self.buffer[file_path] = [line]
+        self.buffer[file_path].append(line)
 
         if len(self.buffer[file_path]) > self.lines_per_file:
             self.flush_file(file_path)
